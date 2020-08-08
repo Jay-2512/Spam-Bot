c309f185de21f63ef29101add5c6eb8ede1b37c6
Jayasurya Jayandan <63704047+Jay-2512@users.noreply.github.com>
2020-08-08 08:50:13 +0530

    Update teapot.py

--- Spammy/teapot.py
+++ Spammy/teapot.py
@@ -1,23 +1,33 @@
-from pynput.keyboard import Key, Controller
+
 from tkinter import *
+from pynput.keyboard import Key,Controller
 import time
 
-keyboard = Controller()
 
-class mainBody:
-    def biscut():
+class mainBody():
+    def biscuit():
+        #--variables--#
+        keyboard = Controller()
+        
+
+
+
+
+        #-- tkinter main window--#
+        
         root = Tk()
-        root.title('Spam Bot')
-        root.geometry("500x500")
+        root.title('')
+        root.geometry('500x550')
         root.resizable(False,False)
 
-        # functions :
-        def start_timer():
-            flag = 0
-            time.sleep(0.5)
-            start_spam(count, message)
-            timer_msg.config(text="Spam Completed \n GG!")
 
+
+        def start_timer():
+            time.sleep(0.5)
+            start_spam(count,message)
+
+
+        #--main function--#
 
         def start_spam(count, message):
             time.sleep(7)
@@ -30,56 +40,109 @@ class mainBody:
                 keyboard.release(Key.enter)
                 lim = lim + 1
 
-        def count_accpt(    ):
+        #--get message--function--#
+        def get_message():
+            global message
+            message = get_messageEntry.get()
+            get_requirementsConfirm.config(text="Got the message!")
+            print("[+] Got the message !")
+
+        #--get count--function--#
+        def get_count():
+            global message
             global count
-            count = 0
-            count = get_count.get()
+            right = False
+            count = get_countEntry.get()
             try:
                 count = int(count)
+                right = True
             except ValueError:
-                msg_conf.config(text="Enter a number -_-").pack()
+                print("[!] YO!! Thats not a number.. such a weirdo..")
+                get_requirementsConfirm.config(text="TBH, That's not a number... Try again")
+            if right == True:
+                get_requirementsConfirm.config(text="Count Accepted!!")
+                time.sleep(1)
+                #print(message)
+                #print(count)
+                print('[+] Got the count!')
+                final_message.config(text="Press the start button and place your cursor at the target location\nSpamming will start soon!")
+                return count
 
-            msg_conf.config(text='Count... Ahah! Got it!!')
-            msg_conf.config(text="All set..\nClick the button to start a 7 sec timer")
-            timer_msg.config(text="Your Spamming will start soon..\n Press the confirm button and \nPlace the cursor where you want to spam")
-            confirm_button = Button(text="Confirm", command=start_timer)
-            confirm_button.pack(pady=10)
+        #--value reset--#
+        def value_reset():
+            global count,message
+            get_countEntry.delete(first=0,last=100)
+            get_messageEntry.delete(first=0, last=100)
+            count = 1
+            message = NONE
+            print("[!!] Erasing all stuffs from the memory! ")
 
+        #--Labels --#
+        heading = Label(root,text="S P A M M Y")
+        heading.config(font=("Courier", 33))
+        heading.pack()
+        footer = Label(root,text="NYX-Tech").pack(side=BOTTOM)
 
-        def msg_accpt():
-            global message
-            message = get_message.get()
-            msg_conf.config(text='Got the message!')
+        #--underline--#
+        underlineLabel = Label(root,text="-------------------------------------")
+        underlineLabel.config(font=("Courier", 44))
+        underlineLabel.pack()
 
-
-
-        # window settings
-        #root.title('Spam Bot')
-        #root.geometry("500x500")
-        #root.resizable(False,False)
-
-        # Label
-        main_label = Label(root,text="SPAMMY").pack()
-        bottom_label = Label(root,text="NYX-Tech").pack(side=BOTTOM)
-
-        #Entry box and other :p :
-
-        l1 = Label(root,text="Enter your message").pack(pady=10)
-        get_message = Entry(root, width='50')
-        get_message.pack(pady=10)
-        accept_message = Button(root, text="Give message",command=msg_accpt)
-        accept_message.pack(pady=10)
-        l2 = Label(root,text="Enter your count").pack(pady=10)
-        get_count = Entry(root, width='50')
-        get_count.pack(pady=10)
-        accept_count = Button(root, text="Give count",command=count_accpt)
-        accept_count.pack(pady=10)
+        #--get message label--#
+        get_messageLabel = Label(root, text="Enter the message : ")
+        get_messageLabel.config(font=("Arial",15))
+        get_messageLabel.pack(pady=10)
 
 
-        # Additional Labels :
 
-        msg_conf = Label(root, text='')
-        msg_conf.pack()
-        timer_msg = Label(root, text='')
-        timer_msg.pack()
+        #--get message entry--#
+
+        get_messageEntry = Entry(root, width="50")
+        get_messageEntry.pack(pady=10)
+
+
+        #--get message button--#
+
+        get_messageButton = Button(root,text="Give Message",command=get_message)
+        get_messageButton.pack()
+
+        #--get count label--#
+        get_countLabel = Label(root, text="Enter the count : ")
+        get_countLabel.config(font=("Arial",15))
+        get_countLabel.pack(pady=10)
+
+        #--get count entry--#
+
+        get_countEntry = Entry(root, width="50")
+        get_countEntry.pack(pady=10)
+
+        #--get count button--#
+
+        get_countButton = Button(root,text="Give Count", command=get_count)
+        get_countButton.pack()
+
+        #--confirmation labels--#
+
+        get_requirementsConfirm = Label(root, text="")
+        get_requirementsConfirm.pack(pady=10)
+
+        final_message = Label(root,text="")
+        final_message.pack(pady=10)
+
+        #-- start timer button--#
+
+        start_timerButton = Button(root, text="Start!", command=start_timer)
+        start_timerButton.pack(pady=20)
+
+        #--reset button--#
+        value_resetButton = Button(root,text="Reset!",command=value_reset)
+        value_resetButton.pack(padx=10, side=LEFT)
+
+        #--exit button--#
+        exit_button = Button(root,text="Exit",command=exit)
+        exit_button.pack(padx=10,side=RIGHT)
+        print("HEYY!!\nVisit our blog : https://nyx-tech@blogspot.com")
+
+
+
         root.mainloop()
